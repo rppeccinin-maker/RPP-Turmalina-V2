0073024c93f47c355194fda1f23b623be4591d6b
Rogério Pereira Peccinin <rppeccinin@gmail.com>
2026-05-19 19:48:41 -0300

    Refactor service worker cache configuration

--- sw.js
+++ sw.js
@@ -1,19 +1,10 @@
-const CACHE_NAME = 'turmalina-cache-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icone.jpg'
-];
+const CACHE_NAME = 'rpp-v1';
+const assets = ['./', './index.html', './manifest.json'];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
